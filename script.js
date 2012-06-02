4de622ecad2c29be7b94faec893dab77bb53d61c
Aaron Roth <aroth3.quentin@gmail.com>
2012-06-02 00:57:19 -0400

    first working version without chords

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-var chordArray = ['A', 'E', 'D'];
-var decisionTime = 1;
+var appendCount = 0;
+var chordArray = [];
+var decisionTime = 0;
+var inputCordArray = ['A', 'D', 'E'];
 var practiceArray = [];
-var practiceCount = 1;
+var practiceCount = 0;
 
 $(document).ready(function() {
+  // Appends checkboxes.
+  for (var i = 0; i < inputCordArray.length; i++) {
+    $('#chords').append(
+      '<label><span class="checkbox">' +
+        '<input class="checkbox-input"' +
+               'type="checkbox"' +
+               'value="' + inputCordArray[i] + '" />' +
+        '<span class="chord-type">' + inputCordArray[i] + '</span>' +
+      '</span></label>');
+  }
+  
   $('.checkbox').live('mouseover', function() {
     $(this).css('background', '#dddddd');
   });
@@ -12,10 +25,36 @@ $(document).ready(function() {
     $(this).css('background', '#ffffff');
   });
   
-  make_practice_array();
-  // practice();
+  $('#start-button').click(function() {
+    var anyChecked = 0;
+    var checkboxes = $('.checkbox-input');
+    
+    for (var i = 0; i < checkboxes.length; i++) {
+      if ($(checkboxes.get(i)).attr('checked')) {
+        anyChecked++;
+      }
+    }
+    
+    if (anyChecked > 0) {
+      get_values();
+
+      $('#main-container').empty();
+      $('#main-container').css('padding','0px');
+      $('#main-container').css('width','464px');
+
+      make_practice_array();
+      practice();
+    } else {
+      alert('Check some chords to practice!');
+    }
+  });
+  
   $('#check-button').live('click', function() {
     check_answers();
+  });
+  
+  $('#reset-button').live('click', function() {
+    window.location.reload();
   });
 });
 
@@ -44,16 +83,34 @@ function check_answers() {
       $(grades.get(i)).text('correct');
       $(grades.get(i)).css('color', '#3fb126');
       $(grades.get(i)).css('font-weight', 'normal');
+      $(grades.get(i)).css('font-style', 'normal');
     } else if (answerGiven == '') {
       $(grades.get(i)).text('incomplete');
       $(grades.get(i)).css('color', '#fe2020');
       $(grades.get(i)).css('font-weight', 'bold');
+      $(grades.get(i)).css('font-style', 'normal');
     } else {
       $(grades.get(i)).text('incorrect');
       $(grades.get(i)).css('color', '#fe2020');
       $(grades.get(i)).css('font-weight', 'bold');
+      $(grades.get(i)).css('font-style', 'normal');
     }
   }
+}
+
+// Retrieves values for practice session.
+function get_values() {
+  var checkboxes = $('.checkbox-input');
+  
+  // Gets chords to practice.
+  for (var i = 0; i < checkboxes.length; i++) {
+    if ($(checkboxes.get(i)).attr('checked')) {
+      chordArray.push($(checkboxes.get(i)).val());
+    }
+  }
+  
+  practiceCount = $('#repetitions-input').val();
+  decisionTime = $('#duration-input').val();
 }
 
 // Makes a chord practice sequence in an array.
@@ -82,14 +139,22 @@ function practice() {
                   '<audio autoplay="autoplay" controls="controls"' +
                          'src="' + sourceURL +'"></audio>' +
                   '<input class="answers" type="text" />' +
-                  '<div class="grades"></div>' +
+                  '<div class="grades">answer</div>' +
                '</div>';  
     $('#main-container').append(html);
+    appendCount++;
+    
+    if (appendCount == 1) {
+      var answers = $('.answers');
+      $(answers.get(0)).focus();
+    }
     
     var timeoutID = window.setTimeout(practice, decisionTime * 1000);
   } else {
-    var html = '<div id="check-button">check</div>';
+    var resetButton = '<div id="reset-button">reset</div>';
+    var checkButton = '<div id="check-button">check</div>';
     
-    $('#main-container').append(html);
+    $('#main-container').append(resetButton);
+    $('#main-container').append(checkButton);
   }
 }
